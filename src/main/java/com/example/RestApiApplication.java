c86d1dfe13e651ef558bccccc28f5270e64c93c4
Nirmal Kumar Sekar <kagosekar@gmail.com>
2019-03-30 18:19:50 +0530

    1. Added eureka client

--- src/main/java/com/example/RestApiApplication.java
+++ src/main/java/com/example/RestApiApplication.java
@@ -3,11 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@EnableEurekaClient
 @EnableCaching
+@SpringBootApplication
 public class RestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
